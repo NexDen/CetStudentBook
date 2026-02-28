16a891a6656f39d36dc5a93ede486db24e35e5f4
nexden <thenexden@gmail.com>
2026-02-28 13:44:27 +0300

    change to postgresql and add book class

--- CetStudentBook/Data/ApplicationDbContext.cs
+++ CetStudentBook/Data/ApplicationDbContext.cs
@@ -6,11 +6,7 @@ namespace CetStudentBook.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
-
         public DbSet<Student> Students { get; set; }
-
-
-
-
+        public DbSet<Book> Books { get; set; }
     }
 }
---
+++ CetStudentBook/Models/Book.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CetStudentBook.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    [Required, StringLength(200, MinimumLength = 2)] public string Name { get; set; }
+    [Required, StringLength(200, MinimumLength = 2)] public string Author { get; set; }
+    [Required] public DateTime PublishDate { get; set; }
+    [Required, Range(1,1000)] public int PageCount  { get; set; }
+    [Required] public bool IsSecondHand { get; set; }
+}
