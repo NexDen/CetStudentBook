f6708eeac34312831fdcee20cae047590fd5144b
nexden <thenexden@gmail.com>
2026-03-01 23:48:27 +0300

    add views and partial api endpoints

--- CetStudentBook/Controllers/BookController.cs
+++ CetStudentBook/Controllers/BookController.cs
@@ -13,11 +13,46 @@ public class BookController : Controller
         _context = context;
     }
 
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
     [HttpPost]
     public IActionResult Create(Book book)
     {
-        
-
+        var tz = book.PublishDate.ToUniversalTime();
+        book.PublishDate = tz; // aşırı garip bir postgresql timestamptz hatasını çözmek için
         _context.Books.Add(book);
+        _context.SaveChangesAsync();
+        return View(book);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Book book)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IActionResult Edit()
+    {
+        return View();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Details(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Index()
+    {
+        var books = _context.Books.ToList();
+        return View(books);
     }
 }
--- CetStudentBook/Models/Book.cs
+++ CetStudentBook/Models/Book.cs
@@ -1,4 +1,5 @@
 
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace CetStudentBook.Models;
@@ -8,7 +9,7 @@ public class Book
     public int Id { get; set; }
     [Required, StringLength(200, MinimumLength = 2)] public string Name { get; set; }
     [Required, StringLength(200, MinimumLength = 2)] public string Author { get; set; }
-    [Required] public DateTime PublishDate { get; set; }
-    [Required, Range(1,1000)] public int PageCount  { get; set; }
-    [Required] public bool IsSecondHand { get; set; }
+    [Required, DisplayName("Publish Date")] public DateTime PublishDate { get; set; }
+    [Required, Range(1,1000), DisplayName("Page Count")] public int PageCount  { get; set; }
+    [Required, DisplayName("Is Second Hand")] public bool IsSecondHand { get; set; }
 }
